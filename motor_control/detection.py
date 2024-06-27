5ef30752fc27de2879b219c301707570afec6d01
Hardik Jain <53528776+HardikJain373@users.noreply.github.com>
2024-06-27 16:45:18 +0530

    second commit

--- motor_control/detection.py
+++ motor_control/detection.py
@@ -8,7 +8,7 @@ from detectionUtils import *
 from ultralytics import YOLO
 
 
-model = YOLO(r"C:/Personal/Robotics_Project/detection/runs\detect/train2/weights/best.pt")
+model = YOLO(r"C:/Personal/Robotics_Project/detection/runs\detect/train3/weights/best.pt")
 
 # height = display_state_from_camera(model, camera_index=0, flip=True, verbose=True)
 t = find_state(model, camera_index=0, flip=True)
